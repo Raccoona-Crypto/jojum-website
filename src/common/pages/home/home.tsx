742f912dfc58fbef434db5bb4a5c0d7d01402f6c
Maksym Tymchyk <m.tymchyk@plark.io>
2020-04-16 19:28:45 +0300

    Update home

--- src/common/pages/home/home.tsx
+++ src/common/pages/home/home.tsx
@@ -4,27 +4,9 @@ import { Section } from 'common/components';
 
 import styles from './home.scss';
 
-const elements = [
-    'Зараз буде останнє слово загадки. Думаю ти знаешь куди його запнути.',
-    'walnut',
-    'Усе що ти розгадав, введи до поля знизу',
-    'Відскануй результат своїм улюбленим гаманцем і не перемикайся',
-    'С днем нарождення!',
-    'Чи тобі мало?',
-    'Далі нічогісінько не буде...',
-    'Стривай!',
-    'А чому б нам не перетворити Jojum на пілотний проект з подарукам в критовалюті?',
-    'Ось концепція, яку ти зараз використовуешь, буде реальним бізнес кейсом.',
-    'Це охуенна ідея, MVP для якої вже існує. Ось воно, перед тобою! Влад і Я досить швидко все імплементували.',
-    'Тому зараз ти бухаеш до посиніння, а завтра сідаємо і обговорюємо як тестувати гіпотезу! ',
-    'З днем народження! Кінець.',
-];
-
 export default function Home(): JSX.Element {
     const [value, setValue] = React.useState('');
     const [step, setStep] = React.useState(-1);
-
-    const totalSteps = elements.length;
 
     const onChangText = React.useCallback((event: any) => {
         setValue(event.target.value);
@@ -37,30 +19,25 @@ export default function Home(): JSX.Element {
     return (
         <>
             <Section>
-                <p>Привіт! Нарешті ти там де треба!</p>
-                <p>Залишилось виконтати декілька степів і приз у тебе в кішені.</p>
-
                 {step < 0 ? (
-                    <div className={styles.interaction}>
-                        <button onClick={setNextStep} className={styles.button}>Розпочати</button>
-                    </div>
-                ) : (
-                    <div className={styles.interaction}>
-                        <p className={styles.stepText}>
-                            <b>Шаг {step + 1}</b> - <span>{elements[step]}</span>
-                        </p>
+                    <>
+                        <p>Привіт, Владику!</p>
+                        <p>Тобі 26 рочків, і ти вже знаешь що робити:</p>
 
-                        {step + 1 >= totalSteps ? undefined : (
-                            <button onClick={setNextStep} className={styles.button}>Наступний шаг</button>
-                        )}
-                    </div>
-                )}
+                        {step < 0 ? (
+                            <div className={styles.interaction}>
+                                <button onClick={setNextStep} className={styles.button}>Показати те що сховано</button>
+                            </div>
+                        ) : undefined}
+                    </>
+                ) : undefined}
 
-                {step > 1 ? (
+
+                {step >= 0 ? (
                     <div>
-                    <textarea onChange={onChangText} placeholder="Відповідь сюди" className={styles.textarea}>
-                        {value}
-                    </textarea>
+                        <textarea onChange={onChangText} placeholder="Відповідь сюди" className={styles.textarea}>
+                            {value}
+                        </textarea>
                     </div>
                 ) : undefined}
 
